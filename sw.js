cb3cf857573ace5e2f3d0c583051e53ac2d9bb95
ezequiellop <62359004+ezequiellop@users.noreply.github.com>
2026-07-23 17:09:21 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='scancheck-v288';
+const CACHE='scancheck-v289';
 const ASSETS=['./','./index.html','./css/style.css','./js/app.js','./js/firebase.js','./js/logo.js','./qr-scanner.html','./manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/jsQR/1.4.0/jsQR.min.js',
   'https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js',
